e41e763d184fbcf8681b62ef2da2de669e5fe406
John KM <jkmaws1@gmail.com>
2024-04-01 22:48:05 +0800

    Update environment

--- DrenchMateWebPrj/src/environments/environment.prod.ts
+++ DrenchMateWebPrj/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:8080'
+    apiUrl: 'http://ec2-18-142-178-165.ap-southeast-1.compute.amazonaws.com:8080'
 };
--- DrenchMateWebPrj/src/environments/environment.ts
+++ DrenchMateWebPrj/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080'
+    apiUrl: 'http://ec2-18-142-178-165.ap-southeast-1.compute.amazonaws.com:8080'
 };
 
 /*
